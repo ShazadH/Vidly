e93d5e8ca38212876f4717be7be7329d492409e0
Shazad Hoang <shazad.hoang@gmail.com>
2020-07-12 21:31:21 +0200

    Conditional Rendering + CSS update

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -12,9 +12,13 @@ class Movies extends Component {
   };
 
   render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
     return (
       <React.Fragment>
-        <table className="table">
+        <p>Showing {count} movies in the database.</p>
+        <table className="table ">
           <thead>
             <tr>
               <th scope="col">Title</th>
