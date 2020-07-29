7f6eaf0590c495d4f4f4258bbaffcfe75ceb32f7
Shazad Hoang <shazad.hoang@gmail.com>
2020-07-29 21:00:13 +0200

    Pagination- Paginating Data

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -3,4 +3,6 @@ import _ from "lodash";
 export function paginate(items, pageNumber, pageSize) {
   const startIndex = (pageNumber - 1) * pageSize;
   return _(items).slice(startIndex).take(pageSize).value();
+
+  // value() return à array
 }
