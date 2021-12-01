dd483f4fe074dafdc5475a8b654fbf1a0b33ada4
Shazad Hoang <shazad.hoang@gmail.com>
2021-12-01 13:02:38 +0100

    Test hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Movies from "./components/movies";
 import "./App.css";
+import Counter from "./components/Hooks/Counter";
 
 function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+    return (
+        <main className="container">
+            <Movies />
+        </main>
+    );
 }
 
 export default App;
--- src/components/Hooks/Counter.jsx
+++ src/components/Hooks/Counter.jsx
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from "react";
 
 const Counter = (props) => {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
 
-  useEffect(() => {
-    document.title = ` ${name} has clicked ${count} times!  `;
+    useEffect(() => {
+        document.title = ` ${name} has clicked ${count} times!  `;
 
-    return () => {
-      console.log("cleanup");
-    };
-  });
+        return () => {
+            console.log("cleanup");
+        };
+    });
 
-  return (
-    <React.Fragment>
-      <div>
-        {name} have clicked {count} times!!!!!{" "}
-      </div>
-      <input onChange={(e) => setName(e.target.value)}></input>
-      <div>Counter: {count}</div>
-      <button onClick={() => setCount(count + 1)}>Button</button>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <div>
+                {name} have clicked {count} times!!!!!{" "}
+            </div>
+            <input onChange={(e) => setName(e.target.value)}></input>
+            <div>Counter: {count}</div>
+            <button onClick={() => setCount(count + 1)}>Button</button>
+        </React.Fragment>
+    );
 };
 
 export default Counter;
