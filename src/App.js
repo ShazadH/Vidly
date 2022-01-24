d053f3165db156079635c302abdb1b6b11435396
Shazad Hoang <shazad.hoang@gmail.com>
2022-01-24 21:21:19 +0100

    Add routing + navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 import "./App.css";
 
 function App() {
     return (
-        <main className="container">
-            <Movies />
-        </main>
+        <React.Fragment>
+            <NavBar />
+            <main className="container">
+                <Switch>
+                    <Route path="/rentals" component={Rentals}></Route>
+                    <Route path="/customers" component={Customers}></Route>
+                    <Route path="/movies" component={Movies}></Route>
+                    <Route path="/not-found" component={NotFound} />
+                    <Redirect from="/" exact to="/movies" />
+                    <Redirect to="not-found" />
+                </Switch>
+            </main>
+        </React.Fragment>
     );
 }
 
