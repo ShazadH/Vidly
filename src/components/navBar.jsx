cd370950886d65f68630ba7a84e8811f9888b05d
Shazad Hoang <shazad.hoang@gmail.com>
2022-01-30 23:10:32 +0100

    Reusable From + Add register page

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -32,6 +32,9 @@ function NavBar() {
                     <NavLink className="nav-link" to="/login">
                         Login
                     </NavLink>
+                    <NavLink className="nav-link" to="/register">
+                        Register
+                    </NavLink>
                 </div>
             </div>
         </nav>
